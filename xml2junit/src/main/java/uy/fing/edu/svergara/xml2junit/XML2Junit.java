a023b2fb64e680d56247e9889621607c1c204ce5
Sebastian Vergara <svergara@uy.ibm.com>
2020-08-28 23:35:13 -0300

    eventb_compliance_v1.0.16

--- xml2junit/src/main/java/uy/fing/edu/svergara/xml2junit/XML2Junit.java
+++ xml2junit/src/main/java/uy/fing/edu/svergara/xml2junit/XML2Junit.java
@@ -3,6 +3,7 @@ package uy.fing.edu.svergara.xml2junit;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
@@ -18,7 +19,6 @@ import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
 import freemarker.template.Configuration;
-import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
 import net.sourceforge.argparse4j.ArgumentParsers;
@@ -42,7 +42,7 @@ public class XML2Junit {
 		parser.addArgument("-t", "--testcases").help("test cases xml file").required(true);
 		try {
 			Namespace ns = parser.parseArgs(args);
-			new XML2Junit().execute(ns.getString("testcases"), ns.getString("strategy"));
+			new XML2Junit().execute(ns.getString("strategy"), ns.getString("testcases"));
 		} catch (ArgumentParserException e) {
 			e.printStackTrace();
 			System.exit(1);
@@ -55,40 +55,116 @@ public class XML2Junit {
 		} catch (TemplateException e) {
 			e.printStackTrace();
 			System.exit(4);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			System.exit(5);
 		}
 	}
 
 	public void execute(String yamlTestGenStrategyPath, String xmlTestCasesPath)
-			throws JAXBException, IOException, TemplateException {
-		xmlTestCasesPath = "/Users/svergara/Box/personales/fing/tesis de maestria/eventb_compliance/eventb2xml/test/20200718T1900_test_cases.xml";
-		yamlTestGenStrategyPath = "/Users/svergara/Desktop/tesis-maestria-eventb-compliance/test/test-gen.yaml";
-		// TODO delete both previous hardcoded values
+			throws JAXBException, IOException, TemplateException, InterruptedException {
 		TestGenStrategy testGenStrategy = parseYamlTestGenStrategyPath(yamlTestGenStrategyPath);
 		ExtendedTestSuite extendedTestSuite = parseTestCasesXml(xmlTestCasesPath);
-		generateJunit(testGenStrategy, extendedTestSuite);
+		createProjectStructure(testGenStrategy);
+		generateContents(testGenStrategy, extendedTestSuite);
 	}
 
-	private void generateJunit(TestGenStrategy testGenStrategy, ExtendedTestSuite extendedTestSuite)
+	private void createProjectStructure(TestGenStrategy testGenStrategy) throws IOException, InterruptedException {
+		// generate default project structure
+		ProcessBuilder processBuilder = new ProcessBuilder("mvn", "archetype:generate",
+				"-DgroupId=" + testGenStrategy.getGroupId(), "-DartifactId=" + testGenStrategy.getArtifactId(),
+				"-DinteractiveMode=false");
+		processBuilder.directory(new File(testGenStrategy.getProjectLocation()));
+		processBuilder.inheritIO();
+		Process process = processBuilder.start();
+		int exitCode = process.waitFor();
+		if (exitCode != 0) {
+			System.out.println("error when executing mvn command");
+			throw new RuntimeException("error creating project structure");
+		}
+		// delete App.java
+		StringBuilder appPath = new StringBuilder();
+		appPath.append(testGenStrategy.getArtifactId()).append(File.separator);
+		appPath.append("src").append(File.separator);
+		appPath.append("main").append(File.separator);
+		appPath.append("java").append(File.separator);
+		appPath.append(testGenStrategy.getGroupId().replaceAll("[.]", File.separator)).append(File.separator);
+		appPath.append("App.java");
+		processBuilder = new ProcessBuilder("rm", "-f", appPath.toString());
+		processBuilder.directory(new File(testGenStrategy.getProjectLocation()));
+		processBuilder.inheritIO();
+		process = processBuilder.start();
+		exitCode = process.waitFor();
+		if (exitCode != 0) {
+			System.out.println("error when executing rm -f App.java");
+			throw new RuntimeException("error creating project structure");
+		}
+		// delete AppTest.java
+		StringBuilder appTestPath = new StringBuilder();
+		appTestPath.append(testGenStrategy.getArtifactId()).append(File.separator);
+		appTestPath.append("src").append(File.separator);
+		appTestPath.append("test").append(File.separator);
+		appTestPath.append("java").append(File.separator);
+		appTestPath.append(testGenStrategy.getGroupId().replaceAll("[.]", File.separator)).append(File.separator);
+		appTestPath.append("AppTest.java");
+		processBuilder = new ProcessBuilder("rm", "-f", appTestPath.toString());
+		processBuilder.directory(new File(testGenStrategy.getProjectLocation()));
+		processBuilder.inheritIO();
+		process = processBuilder.start();
+		exitCode = process.waitFor();
+		if (exitCode != 0) {
+			System.out.println("error when executing rm -f AppTest.java");
+			throw new RuntimeException("error creating project structure");
+		}
+	}
+
+	private void generateContents(TestGenStrategy testGenStrategy, ExtendedTestSuite extendedTestSuite)
 			throws IOException, TemplateException {
 		Configuration configuration = setupFreemarker();
-		Writer out = new OutputStreamWriter(System.out);
-		System.out.println("--------------------------------------------------------");
+		System.out.println("Wrapper.java --------------------------------------------------------");
+		StringBuilder wrapperPath = new StringBuilder();
+		wrapperPath.append(testGenStrategy.getProjectLocation()).append(File.separator);
+		wrapperPath.append(testGenStrategy.getArtifactId()).append(File.separator);
+		wrapperPath.append("src").append(File.separator);
+		wrapperPath.append("main").append(File.separator);
+		wrapperPath.append("java").append(File.separator);
+		wrapperPath.append(testGenStrategy.getGroupId().replaceAll("[.]", File.separator)).append(File.separator);
+		wrapperPath.append("Wrapper.java");
 		FreemarkerWrapperModel freemarkerWrapperModel = FreeMarkerModelBuilderFactory.instance()
 				.buildFreemarkerWrapperDataModel(testGenStrategy, extendedTestSuite);
-		configuration.getTemplate("Wrapper.ftl").process(freemarkerWrapperModel, out);
-		System.out.println("--------------------------------------------------------");
+		configuration.getTemplate("Wrapper.ftl").process(freemarkerWrapperModel,
+				new FileWriter(wrapperPath.toString()));
+		System.out.println("Types --------------------------------------------------------");
+		StringBuilder typePath = null;
 		for (Type aType : testGenStrategy.getTypes()) {
 			if ("enum".equalsIgnoreCase(aType.getSupertype())) {
+				typePath = new StringBuilder();
+				typePath.append(testGenStrategy.getProjectLocation()).append(File.separator);
+				typePath.append(testGenStrategy.getArtifactId()).append(File.separator);
+				typePath.append("src").append(File.separator);
+				typePath.append("main").append(File.separator);
+				typePath.append("java").append(File.separator);
+				typePath.append(testGenStrategy.getGroupId().replaceAll("[.]", File.separator)).append(File.separator);
+				typePath.append(aType.getName()).append(".java");
 				configuration.getTemplate("Enum.ftl").process(
 						FreeMarkerModelBuilderFactory.instance().buildFreemarkerEnumrDataModel(testGenStrategy, aType),
-						out);
+						new FileWriter(typePath.toString()));
 			} else {
 				throw new RuntimeException(aType.getName() + " supertype attribute is not valid");
 			}
 		}
-		System.out.println("--------------------------------------------------------");
+		System.out.println("WrapperTest.java --------------------------------------------------------");
+		StringBuilder wrapperTestPath = new StringBuilder();
+		wrapperTestPath.append(testGenStrategy.getProjectLocation()).append(File.separator);
+		wrapperTestPath.append(testGenStrategy.getArtifactId()).append(File.separator);
+		wrapperTestPath.append("src").append(File.separator);
+		wrapperTestPath.append("test").append(File.separator);
+		wrapperTestPath.append("java").append(File.separator);
+		wrapperTestPath.append(testGenStrategy.getGroupId().replaceAll("[.]", File.separator)).append(File.separator);
+		wrapperTestPath.append("WrapperTest.java");
 		configuration.getTemplate("WrapperTest.ftl").process(FreeMarkerModelBuilderFactory.instance()
-				.buildFreemarkerWrapperTestDataModel(testGenStrategy, extendedTestSuite, freemarkerWrapperModel), out);
+				.buildFreemarkerWrapperTestDataModel(testGenStrategy, extendedTestSuite, freemarkerWrapperModel),
+				new FileWriter(wrapperTestPath.toString()));
 		System.out.println("--------------------------------------------------------");
 	}
 
